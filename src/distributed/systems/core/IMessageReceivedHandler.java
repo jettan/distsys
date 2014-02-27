6eb6f40673b3747dd3237e976911aba666910849
qstokkink <goqs@hotmail.com>
2014-02-27 09:10:27 +0100

    Added Message and MessageReceivedHandler contents

--- src/distributed/systems/core/IMessageReceivedHandler.java
+++ src/distributed/systems/core/IMessageReceivedHandler.java
@@ -1,5 +1,16 @@
 package distributed.systems.core;
 
+/**
+ * Interface for classes that can handle incoming Message
+ * objects.
+ */
 public interface IMessageReceivedHandler {
 
+	/**
+	 * Receive a message
+	 * 
+	 * @param message The message that was sent to us
+	 */
+	public void onMessageReceived(Message message);
+	
 }
--- src/distributed/systems/core/Message.java
+++ src/distributed/systems/core/Message.java
@@ -1,15 +1,44 @@
 package distributed.systems.core;
 
-public class Message {
+import java.io.Serializable;
+import java.util.HashMap;
 
-	public Object get(String string) {
-		// TODO Auto-generated method stub
-		return null;
+public class Message implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * We use a Serializable here instead of an Object
+	 * to benefit from static type checking (instead of
+	 * the NotSerializable runtime exception).
+	 */
+	private HashMap<String, Serializable> messageContent;
+	
+	/**
+	 * Construct a new Message keystore
+	 */
+	public Message(){
+		messageContent = new HashMap<String, Serializable>();
+	}
+	
+	/**
+	 * Get the value corresponding to a certain key
+	 * 
+	 * @param string The key to retrieve
+	 * @return The value corresponding to the key
+	 */
+	public Serializable get(String string) {
+		return messageContent.get(string);
 	}
 
-	public void put(String string, Object object) {
-		// TODO Auto-generated method stub
-		
+	/**
+	 * Add a new key->value pair to our Message content
+	 * 
+	 * @param string The key
+	 * @param object The corresponding value
+	 */
+	public void put(String string, Serializable object) {
+		messageContent.put(string, object);
 	}
 
 }
