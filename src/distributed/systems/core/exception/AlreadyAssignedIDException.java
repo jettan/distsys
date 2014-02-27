38683ec7c78bfb12bbb081b3ff673eb9ed1ece07
qstokkink <goqs@hotmail.com>
2014-02-27 09:25:44 +0100

    Implemented exceptions

--- src/distributed/systems/core/exception/AlreadyAssignedIDException.java
+++ src/distributed/systems/core/exception/AlreadyAssignedIDException.java
@@ -1,5 +1,11 @@
 package distributed.systems.core.exception;
 
+/**
+ * An Exception thrown when a Socket tries to register
+ * a serverid that is already in use.
+ */
 public class AlreadyAssignedIDException extends Exception {
+
+	private static final long serialVersionUID = 1L;
 
 }
--- src/distributed/systems/core/exception/IDNotAssignedException.java
+++ src/distributed/systems/core/exception/IDNotAssignedException.java
@@ -1,5 +1,11 @@
 package distributed.systems.core.exception;
 
+/**
+ * An Exception thrown when a Socket tries to send
+ * a message to a non-existent serverid.
+ */
 public class IDNotAssignedException extends Exception {
+
+	private static final long serialVersionUID = 1L;
 
 }
