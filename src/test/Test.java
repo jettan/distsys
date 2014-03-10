0a6306d893aa892f85eebf58c35854d912210da5
qstokkink <goqs@hotmail.com>
2014-03-10 11:27:52 +0100

    Made test example more general

--- src/test/Test.java
+++ src/test/Test.java
@@ -12,17 +12,27 @@ import distributed.systems.example.LocalSocket;
 public class Test implements IMessageReceivedHandler{
 
 	public static void main(String[] args) throws RemoteException, AlreadyAssignedIDException, IDNotAssignedException{
+		// shared
 		Registry reg = java.rmi.registry.LocateRegistry.createRegistry(1099);
 		
-		LocalSocket ls = new LocalSocket();
-		ls.register("1234");
-		ls.addMessageReceivedHandler(new Test());
+		// SERVER code
+		LocalSocket srv = new LocalSocket();
+		srv.register("1234");
 		
-		ls.sendMessage(new Message(), "1234");
-		ls.unRegister();
+		// CLIENT code
+		LocalSocket clt = new LocalSocket();
+		clt.register("5678");
+		clt.addMessageReceivedHandler(new Test());
 		
+		// SERVER code
+		srv.sendMessage(new Message(), "5678");
+		
+		// shared
+		srv.unRegister();
+		clt.unRegister();
+	
+		// shared
 		java.rmi.server.UnicastRemoteObject.unexportObject(reg,true);
-		
 		System.exit(0);
 	}
 
