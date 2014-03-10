d93c95d12032297ea2d90801d20ae731aeade52a
qstokkink <goqs@hotmail.com>
2014-03-10 11:23:54 +0100

    Made Test implement IMessageReceivedHandler

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,18 +3,20 @@ package test;
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
 
+import distributed.systems.core.IMessageReceivedHandler;
 import distributed.systems.core.Message;
 import distributed.systems.core.exception.AlreadyAssignedIDException;
 import distributed.systems.core.exception.IDNotAssignedException;
 import distributed.systems.example.LocalSocket;
 
-public class Test {
+public class Test implements IMessageReceivedHandler{
 
 	public static void main(String[] args) throws RemoteException, AlreadyAssignedIDException, IDNotAssignedException{
 		Registry reg = java.rmi.registry.LocateRegistry.createRegistry(1099);
 		
 		LocalSocket ls = new LocalSocket();
 		ls.register("1234");
+		ls.addMessageReceivedHandler(new Test());
 		
 		ls.sendMessage(new Message(), "1234");
 		ls.unRegister();
@@ -22,6 +24,11 @@ public class Test {
 		java.rmi.server.UnicastRemoteObject.unexportObject(reg,true);
 		
 		System.exit(0);
+	}
+
+	@Override
+	public void onMessageReceived(Message message) throws RemoteException {
+		System.out.println("I received a message! :)");
 	}
 	
 }
